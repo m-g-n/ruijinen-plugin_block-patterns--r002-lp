4a44917340b896ff9069d1d2e32875e6cfe4e4a1
アクツユミ <akutsu@m-g-n.me>
2021-06-11 16:59:53 +0900

    ファイル・スタイルのリファクタリングなど

--- patterns/RJE_R002LP_hero_one_column/pattern.php
+++ patterns/RJE_R002LP_hero_one_column/pattern.php
@@ -1,10 +1,10 @@
-<!-- wp:snow-monkey-blocks/section-with-bgimage {"titleTagName":"h1","lgImageID":39,"lgImageMediaType":"image","height":"wide","contentsAlignment":"center","align":"full","className":"is-style-RJE_R002LP_hero_one_column"} -->
-<div class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section-with-bgimage--center smb-section-with-bgimage--wide is-style-RJE_R002LP_hero_one_column"><div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg"><img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/bg-scaled.jpg" alt="" class="wp-image-39" style="opacity:1"/></div><div class="c-container"><h1 class="smb-section__title">思い通りにならない髪に、<br>さようなら。</h1><div class="smb-section__body"><!-- wp:paragraph -->
+<!-- wp:snow-monkey-blocks/section-with-bgimage {"titleTagName":"h1","lgImageID":39,"lgImageMediaType":"image","height":"wide","contentsAlignment":"center","align":"full","className":"RJE_R002LP_hero_one_column is-style-RJE_R002LP_hero_one_column"} -->
+<div class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section-with-bgimage--center smb-section-with-bgimage--wide RJE_R002LP_hero_one_column is-style-RJE_R002LP_hero_one_column"><div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/background1.jpg" alt="" class="wp-image-39" style="opacity:1"/></div><div class="c-container"><h1 class="smb-section__title">思い通りにならない髪に、<br>さようなら。</h1><div class="smb-section__body"><!-- wp:paragraph -->
 <p>あなたの髪質に合わせた<br>シャンプーのパーソナライズサービス</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:image {"id":182,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/MATCH.png" alt="" class="wp-image-182"/></figure>
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/logo1.png" alt="" class="wp-image-182"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:snow-monkey-blocks/buttons {"contentJustification":"center"} -->
