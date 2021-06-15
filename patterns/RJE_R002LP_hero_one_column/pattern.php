c4fe1cab1d9a949fbb31e36d5834d447dc00037e
アクツユミ <akutsu@m-g-n.me>
2021-06-15 13:25:44 +0900

    RUIJINEN-411 [LP：パターン] 作成パターンの修正
    - スタイルの微調整
    - お試し版に同梱するパターンの画像をCC0画像に差し替え

--- patterns/RJE_R002LP_hero_one_column/pattern.php
+++ patterns/RJE_R002LP_hero_one_column/pattern.php
@@ -1,15 +1,15 @@
-<!-- wp:snow-monkey-blocks/section-with-bgimage {"titleTagName":"h1","lgImageID":39,"lgImageMediaType":"image","height":"wide","contentsAlignment":"center","align":"full","className":"RJE_R002LP_hero_one_column is-style-RJE_R002LP_hero_one_column"} -->
-<div class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section-with-bgimage--center smb-section-with-bgimage--wide RJE_R002LP_hero_one_column is-style-RJE_R002LP_hero_one_column"><div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/background1.jpg" alt="" class="wp-image-39" style="opacity:1"/></div><div class="c-container"><h1 class="smb-section__title">思い通りにならない髪に、<br>さようなら。</h1><div class="smb-section__body"><!-- wp:paragraph -->
+<!-- wp:snow-monkey-blocks/section-with-bgimage {"titleTagName":"h1","lgImageID":465,"lgImageMediaType":"image","height":"wide","contentsAlignment":"center","maskColor":"#000","maskOpacity":0.4,"textColor":"#fff","align":"full","className":"RJE_R002LP_hero_one_column is-style-RJE_R002LP_hero_one_column"} -->
+<div class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section-with-bgimage--center smb-section-with-bgimage--wide RJE_R002LP_hero_one_column is-style-RJE_R002LP_hero_one_column" style="color:#fff"><div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg"><div class="smb-section-with-bgimage__mask" style="background-color:#000"></div><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/photo-woman1.jpg" alt="" class="wp-image-465" style="opacity:0.4"/></div><div class="c-container"><h1 class="smb-section__title">思い通りにならない髪に、<br>さようなら。</h1><div class="smb-section__body"><!-- wp:paragraph -->
 <p>あなたの髪質に合わせた<br>シャンプーのパーソナライズサービス</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:image {"id":182,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/logo1.png" alt="" class="wp-image-182"/></figure>
+<!-- wp:image {"id":488,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/logo1-white.png" alt="" class="wp-image-488"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:snow-monkey-blocks/buttons {"contentJustification":"center"} -->
-<div class="wp-block-snow-monkey-blocks-buttons smb-buttons is-content-justification-center"><!-- wp:snow-monkey-blocks/btn {"url":"#","className":"is-style-ghost"} -->
-<div class="wp-block-snow-monkey-blocks-btn smb-btn-wrapper is-style-ghost"><a class="smb-btn" href="#"><span class="smb-btn__label">診断する</span></a></div>
+<div class="wp-block-snow-monkey-blocks-buttons smb-buttons is-content-justification-center"><!-- wp:snow-monkey-blocks/btn {"url":"#","backgroundColor":"#fff","textColor":"#fff","className":"is-style-ghost"} -->
+<div class="wp-block-snow-monkey-blocks-btn smb-btn-wrapper is-style-ghost"><a class="smb-btn" href="#" style="background-color:#fff;border-color:#fff"><span class="smb-btn__label" style="color:#fff">診断する</span></a></div>
 <!-- /wp:snow-monkey-blocks/btn --></div>
 <!-- /wp:snow-monkey-blocks/buttons --></div></div></div>
 <!-- /wp:snow-monkey-blocks/section-with-bgimage -->
