808c3fcc2afc1cd2c877d4627cfe5446eea24a8e
アクツユミ <akutsu@m-g-n.me>
2021-07-21 14:04:44 +0900

    パターンHTMLを追加

--- patterns/RJE_R002LP_prices/pattern.php
+++ patterns/RJE_R002LP_prices/pattern.php
@@ -1,1 +1,19 @@
-<!-- あとで入れる -->
+<!-- wp:snow-monkey-blocks/section-with-bgimage {"lgImageID":28,"lgImageMediaType":"image","contentsAlignment":"center","align":"full","className":"RJE_P002LP_prices is-style-RJE_R002LP_section1"} -->
+<div class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section--center smb-section--fit RJE_P002LP_prices is-style-RJE_R002LP_section1"><div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/transparent.png" alt="" class="wp-image-28" style="opacity:1"/></div><div class="smb-section__inner"><div class="c-container"><div class="smb-section__subtitle">Plan</div><h2 class="smb-section__title">購入プラン</h2><div class="smb-section__body"><!-- wp:snow-monkey-blocks/pricing-table {"columnSize":"1-3","childrenCount":3,"className":"is-style-RJE_R002LP_price_list"} -->
+<div class="wp-block-snow-monkey-blocks-pricing-table smb-pricing-table smb-pricing-table--col-size-1-3 is-style-RJE_R002LP_price_list" data-has-items="3"><div class="c-row c-row--md-nowrap"><!-- wp:snow-monkey-blocks/pricing-table--item -->
+<div class="wp-block-snow-monkey-blocks-pricing-table--item c-row__col"><div class="smb-pricing-table__item"><div class="smb-pricing-table__item__title">単回購入プラン</div><div class="smb-pricing-table__item__price">3,200円</div><div class="smb-pricing-table__item__lede">まずは試したい方に</div><ul><li>MATCHが自分に合うかどうか、まずはお試ししてみたい方に。<br>1包装の購入後、継続するかどうかお決めいただけます。</li></ul></div></div>
+<!-- /wp:snow-monkey-blocks/pricing-table--item -->
+
+<!-- wp:snow-monkey-blocks/pricing-table--item -->
+<div class="wp-block-snow-monkey-blocks-pricing-table--item c-row__col"><div class="smb-pricing-table__item"><div class="smb-pricing-table__item__title">継続購入プラン</div><div class="smb-pricing-table__item__price">2,980円</div><div class="smb-pricing-table__item__lede">髪質の変化を継続実感</div><ul><li>毎月、継続でMTACHのシャンプーが届きます。<br>単回購入よりも、220円お得です。</li></ul></div></div>
+<!-- /wp:snow-monkey-blocks/pricing-table--item -->
+
+<!-- wp:snow-monkey-blocks/pricing-table--item -->
+<div class="wp-block-snow-monkey-blocks-pricing-table--item c-row__col"><div class="smb-pricing-table__item"><div class="smb-pricing-table__item__title">まとめ払いプラン</div><div class="smb-pricing-table__item__price">2,780円</div><div class="smb-pricing-table__item__lede">1年分先払いのお得プラン</div><ul><li>継続購入プランを、1年間分まとめてお支払いいただきます。<br>継続購入プランよりも、月々200円（年間2,400円）お得です。</li></ul></div></div>
+<!-- /wp:snow-monkey-blocks/pricing-table--item --></div></div>
+<!-- /wp:snow-monkey-blocks/pricing-table -->
+
+<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":14}}} -->
+<p class="has-text-align-right" style="font-size:14px">*1包装 = 500ml<br>*税抜き価格です</p>
+<!-- /wp:paragraph --></div></div></div></div>
+<!-- /wp:snow-monkey-blocks/section-with-bgimage -->
