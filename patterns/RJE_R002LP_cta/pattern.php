8bd85a303149999bdb5bb4a753a84c7aaa427659
アクツユミ <akutsu@m-g-n.me>
2021-07-26 15:13:46 +0900

    CTAパターン作成

--- patterns/RJE_R002LP_cta/pattern.php
+++ patterns/RJE_R002LP_cta/pattern.php
@@ -1,1 +1,13 @@
-<!-- あとで -->
+<!-- wp:snow-monkey-blocks/section {"titleTagName":"none","fixedBackgroundColor":"#999999","textColor":"#fff","align":"full","className":"is-style-RJE_R002LP_cta"} -->
+<div class="wp-block-snow-monkey-blocks-section alignfull smb-section smb-section--fit is-style-RJE_R002LP_cta" style="color:#fff"><div class="smb-section__fixed-background" style="padding-top:0;padding-bottom:0;background-color:#999999"></div><div class="smb-section__inner"><div class="c-container"><div class="smb-section__body"><!-- wp:snow-monkey-blocks/media-text {"mediaId":1007,"mediaLink":"https://rui-jin-en.com/","mediaSizeSlug":"full","mediaType":"image","mediaPosition":"left","mobileOrder":"image"} -->
+<div class="wp-block-snow-monkey-blocks-media-text smb-media-text smb-media-text--mobile-image"><div class="c-row c-row--margin c-row--reverse c-row--middle"><div class="c-row__col c-row__col--1-1 c-row__col--lg-1-3"><h2 class="smb-media-text__title">まずは、診断から始めましょう。</h2><div class="smb-media-text__body"><!-- wp:paragraph -->
+<p>診断は10分ほどで終わります。<br>あなたに最適なシャンプーとの出会いをお楽しみください。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:snow-monkey-blocks/buttons -->
+<div class="wp-block-snow-monkey-blocks-buttons smb-buttons"><!-- wp:snow-monkey-blocks/btn {"url":"#hoge","backgroundColor":"#fff","textColor":"#313131","className":"is-style-default"} -->
+<div class="wp-block-snow-monkey-blocks-btn smb-btn-wrapper is-style-default"><a class="smb-btn" href="#hoge" style="background-color:#fff"><span class="smb-btn__label" style="color:#313131">診断する</span></a></div>
+<!-- /wp:snow-monkey-blocks/btn --></div>
+<!-- /wp:snow-monkey-blocks/buttons --></div></div><div class="c-row__col c-row__col--1-1 c-row__col--lg-2-3"><div class="smb-media-text__figure"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/photo-product3.png" alt="" width="408" height="572" class="wp-image-1007"/></div></div></div></div>
+<!-- /wp:snow-monkey-blocks/media-text --></div></div></div></div>
+<!-- /wp:snow-monkey-blocks/section -->
