9703cf1002bb9a8f7913f2332eaf82963e71b794
アクツユミ <akutsu@m-g-n.me>
2021-08-10 16:00:39 +0900

    バグ対応

--- patterns/RJE_R002LP_cta/pattern.php
+++ patterns/RJE_R002LP_cta/pattern.php
@@ -1,5 +1,5 @@
-<!-- wp:snow-monkey-blocks/section {"titleTagName":"none","fixedBackgroundColor":"#999999","textColor":"#fff","align":"full","className":"is-style-RJE_R002LP_cta"} -->
-<div class="wp-block-snow-monkey-blocks-section alignfull smb-section smb-section--fit is-style-RJE_R002LP_cta" style="color:#fff"><div class="smb-section__fixed-background" style="padding-top:0;padding-bottom:0;background-color:#999999"></div><div class="smb-section__inner"><div class="c-container"><div class="smb-section__body"><!-- wp:snow-monkey-blocks/media-text {"mediaId":1007,"mediaLink":"https://rui-jin-en.com/","mediaSizeSlug":"full","mediaType":"image","mediaPosition":"left","mobileOrder":"image"} -->
+<!-- wp:snow-monkey-blocks/section-with-bgimage {"titleTagName":"none","lgImageID":28,"lgImageMediaType":"image","maskColor":"#f7f7f7","maskOpacity":0,"align":"full","className":"is-style-RJE_R002LP_cta"} -->
+<div class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section--left smb-section--fit is-style-RJE_R002LP_cta"><div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg"><div class="smb-section-with-bgimage__mask" style="background-color:#f7f7f7"></div><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/transparent.png" alt="" class="wp-image-28" style="opacity:0"/></div><div class="smb-section__inner"><div class="c-container"><div class="smb-section__body"><!-- wp:snow-monkey-blocks/media-text {"mediaId":1007,"mediaLink":"https://rui-jin-en.com/","mediaSizeSlug":"full","mediaType":"image","mediaPosition":"left","mobileOrder":"image"} -->
 <div class="wp-block-snow-monkey-blocks-media-text smb-media-text smb-media-text--mobile-image"><div class="c-row c-row--margin c-row--reverse c-row--middle"><div class="c-row__col c-row__col--1-1 c-row__col--lg-1-3"><h2 class="smb-media-text__title">まずは、診断から始めましょう。</h2><div class="smb-media-text__body"><!-- wp:paragraph -->
 <p>診断は10分ほどで終わります。<br>あなたに最適なシャンプーとの出会いをお楽しみください。</p>
 <!-- /wp:paragraph -->
@@ -10,4 +10,4 @@
 <!-- /wp:snow-monkey-blocks/btn --></div>
 <!-- /wp:snow-monkey-blocks/buttons --></div></div><div class="c-row__col c-row__col--1-1 c-row__col--lg-2-3"><div class="smb-media-text__figure"><img src="<?php echo esc_url( RJE_R002LP_URL ); ?>dist/img/photo-product3.png" alt="" width="408" height="572" class="wp-image-1007"/></div></div></div></div>
 <!-- /wp:snow-monkey-blocks/media-text --></div></div></div></div>
-<!-- /wp:snow-monkey-blocks/section -->
+<!-- /wp:snow-monkey-blocks/section-with-bgimage -->
--- patterns/RJE_R002LP_cta_fix/script-front.js
+++ patterns/RJE_R002LP_cta_fix/script-front.js
@@ -1,17 +1,69 @@
-/**
-* CTA固定パターンに閉じるボタンを付与し、ボタンクリック時に非表示classを付与
-*/
-
-window.onload = function() {
-	let cta = document.getElementsByClassName('is-style-RJE_R002LP_cta_fix')[0];
-	let cta_close = '';
-	let close_html = '<button aria-label="閉じる" class="close-btn">×</button>';
-	if ( cta ) {
-		cta = cta.getElementsByClassName('smb-items__item')[0];
-		cta.insertAdjacentHTML('beforeend', close_html);
-		cta_close = cta.getElementsByClassName('close-btn')[0];
-		cta_close.onclick = function changeContent() {
-			cta.classList.add('is-hidden');
+window.addEventListener(
+	'load',
+	() => {
+		
+		let cta = document.getElementsByClassName('is-style-RJE_R002LP_cta_fix')[0];
+		let cta_close = '';
+		const close_html = '<button aria-label="閉じる" class="close-btn">×</button>';
+		const pageTop = document.getElementById('page-top');
+		if ( cta ) {
+			//閉じるボタンの付与、ボタンクリック時に非表示classを付与
+			let cta_item = cta.getElementsByClassName('smb-items__item')[0];
+			cta_item.insertAdjacentHTML('beforeend', close_html);
+			cta_close = cta_item.getElementsByClassName('close-btn')[0];
+			cta_close.onclick = function changeContent() {
+				cta_item.classList.add('is-hidden');
+			}
+			//ページトップボタンが表示された場合は表示位置をコントロールするclassを付与
+			if (! pageTop) {
+				return;
+			}
+			const handleScroll = throttle(
+				() => {
+					if (500 <= window.pageYOffset) {
+						cta.classList.add('is-visible_page_top');
+					} else {
+						cta.classList.remove('is-visible_page_top');
+					}
+				},
+				150
+			);
+			window.addEventListener('scroll', handleScroll, isPassiveSupported() ? { passive: true } : false);
 		}
-	}
-};
+	},
+	false
+);
+
+//ヘルパー関数 - Snow Monkeyから流用
+function throttle(callback, delay) {
+  let time = Date.now();
+  return () => {
+    if ((time + delay - Date.now()) < 0) {
+      callback.apply(this, arguments);
+      time = Date.now();
+    }
+  };
+}
+
+function isPassiveSupported() {
+  let passiveSupported = false;
+
+  try {
+    const options = Object.defineProperty(
+      {},
+      'passive',
+      {
+        get: () => {
+          passiveSupported = true;
+        }
+      }
+    );
+
+    window.addEventListener('test', options, options);
+    window.removeEventListener('test', options, options);
+  } catch(err) {
+    passiveSupported = false;
+  }
+
+  return passiveSupported;
+}
