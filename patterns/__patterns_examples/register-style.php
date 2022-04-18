a897f631d42f83959f254b9e656045182015713f
アクツユミ <akutsu@m-g-n.me>
2022-04-18 19:03:21 +0900

    Snow MonkeyのCSSファイルのハンドル取得のプロパティの変更 #158

--- patterns/__patterns_examples/register-style.php
+++ patterns/__patterns_examples/register-style.php
@@ -13,5 +13,5 @@ $front_filename = 'dist/css/patterns/' . $basename . '/style-front.css';
 $editor_filename = 'dist/css/patterns/' . $basename . '/style-editor.css';
 
 //フロント・エディター用のCSSファイルを登録
-wp_register_style( $basename . '-front', RJE_R002LP_URL . $front_filename, $this->style_front_deps, filemtime( RJE_R002LP_PATH . $front_filename ) );
-wp_register_style( $basename . '-editor', RJE_R002LP_URL . $editor_filename, $this->style_editor_deps, filemtime( RJE_R002LP_PATH . $editor_filename ) );
+wp_register_style( $basename . '-front', RJE_R002LP_URL . $front_filename, $this->sm_style_handles, filemtime( RJE_R002LP_PATH . $front_filename ) );
+wp_register_style( $basename . '-editor', RJE_R002LP_URL . $editor_filename, $this->sm_style_handles, filemtime( RJE_R002LP_PATH . $editor_filename ) );
